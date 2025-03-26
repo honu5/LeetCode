bb0144e35b6f7fe5470a7cfe25c105579d46e9e2
Honelgn Yohannes <liranso392@gmail.com>
2025-03-26 09:14:17 -0700

    Time: 103 ms (97.37%), Space: 38.7 MB (97.37%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -1,8 +1,9 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         list1=[num for row in grid for num in row]
+        b=grid[0][0]%x
         for i in list1:
-            if i%x!=0:
+            if i%x!=b:
                 return -1
         list1.sort()
         a=0
