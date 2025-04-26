b539bddc5df20d9c558d3a41eab5cf2a60c935b8
Honelgn Yohannes <liranso392@gmail.com>
2025-04-26 06:32:20 -0700

    Time: 4 ms (24.8%), Space: 17.8 MB (35.52%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,9 +1,8 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         
-        if nums[0]==100:
-            return 100
-        for i in range(max(nums)):
+        
+        for i in range(max(nums)+1):
             cnt=0
             for j in range(len(nums)):
                 if nums[j]>=i:
