82e9f583335f1aa85dde36381e2471aa5476c127
Honelgn Yohannes <liranso392@gmail.com>
2025-05-03 14:42:00 +0300

    Time: 48 ms (50.74%), Space: 23.4 MB (22.17%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -4,17 +4,17 @@ class Solution:
         l = 0
         r = len(people) - 1
         boat=0
-        ans = []
+        
 
         while l <= r:
 
             if people[r] + people[l] > limit:
-                ans.append([people[r]])
+                
                 r -= 1
                 boat+=1
 
             else:
-                ans.extend([[people[l], people[r]]])
+                
                 l += 1
                 r -= 1
                 boat+=1
