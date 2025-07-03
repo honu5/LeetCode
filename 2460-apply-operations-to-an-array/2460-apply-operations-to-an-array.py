d579aca2eabc8ac810a0585705132aa96bd58a1f
Honelgn Yohannes <liranso392@gmail.com>
2025-07-03 01:06:54 -0700

    Time: 31 ms (8.06%), Space: 12.7 MB (16.12%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -4,16 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        non_zero_idx = 0
-
-        # Apply operations
+        n=len(nums)-1
         for i in range(n):
-            if i < n - 1 and nums[i] == nums[i + 1]:
-                nums[i] *= 2
-                nums[i + 1] = 0
-            if nums[i] != 0:
-                nums[i], nums[non_zero_idx] = nums[non_zero_idx], nums[i]
-                non_zero_idx += 1
+            if nums[i]==nums[i+1]:
+                nums[i],nums[i+1]=nums[i]*2,0
 
+        for i in nums:
+            if i==0:
+                nums.remove(i)
+                nums.append(i)
         return nums
+                
+
+        
+
+        
+        
