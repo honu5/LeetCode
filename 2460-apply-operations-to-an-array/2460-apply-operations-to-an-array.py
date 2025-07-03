6df879b4165b0b505cf81e9e0f5b4fabcf5c300e
Honelgn Yohannes <liranso392@gmail.com>
2025-07-03 01:13:37 -0700

    Time: 2 ms (51.64%), Space: 12.6 MB (51.64%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -8,12 +8,13 @@ class Solution(object):
         non_zero=0
         for i in range(n):
             if i<n-1 and nums[i]==nums[i+1]:
-                nums[i],nums[i+1]=nums[i]*2,0
+                nums[i]=nums[i]*2
+                nums[i+1]=0
             if nums[i]!=0:
                 nums[i],nums[non_zero]=nums[non_zero],nums[i]
                 non_zero+=1
 
-        print(nums)
+        
         return nums
                 
 
