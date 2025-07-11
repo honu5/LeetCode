c2514d704d18067bab91f1fb7f2f6007e2a87644
Honelgn Yohannes <liranso392@gmail.com>
2025-07-11 04:03:58 -0700

    Time: 16 ms (23.93%), Space: 15.5 MB (6.33%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,43 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count = set()
-        for i in xrange(len(nums)):
-            if nums[i] in count:
-                count.remove(nums[i])
-            else:
-                count.add(nums[i])
-        return count.pop()
+        c=Counter(nums)
+        d=c.most_common()[-1]
+        print(d)
+        return d[0]
 
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        return 2*sum(set(nums)) - sum(nums)
-
-
-# method 2, use dict, O(n) space
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = {}
-        for i in xrange(len(nums)):
-            count[nums[i]] = count.get(nums[i], 0) + 1
-        for k,v in count.items():
-            if v == 1:
-                return k
-
-# method 3,xor, O(1) space
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        return reduce(lambda x,y: x^y, nums)
-            
+        
+        
