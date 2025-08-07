60d95a39e43f0b036abe5029d4bfec4c5c225dfd
Honelgn Yohannes <liranso392@gmail.com>
2025-08-07 15:48:53 +0300

    Time: 379 ms (5.35%), Space: 111.8 MB (5.35%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -4,21 +4,7 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        '''ad=0
-        if num==33550336:
-            return True
-        if num>=10000:
-            return False
-        if num==1:
-            return False
-        if num%2!=0:
-            return False
-        for i in range(1,num//2+1):
-            if num%i==0:
-                ad+=i
-        if ad==num:
-            return True
-        return False'''
+        
         ans=[1]
         if num%2!=0:
             return False
