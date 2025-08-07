15d0491f1c161bfe7f06a4dd8992a62c1629f8c1
Honelgn Yohannes <liranso392@gmail.com>
2025-08-07 15:35:21 +0300

    Time: 0 ms (100%), Space: 12.5 MB (75.79%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -9,8 +9,7 @@ class Solution(object):
             return True
         if num>=10000:
             return False
-        if num==1:
-            return False
+        
         if num%2!=0:
             return False
         for i in range(1,num//2+1):
