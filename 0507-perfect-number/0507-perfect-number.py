635b25bea184d91c81f1651509305517cad17790
Honelgn Yohannes <liranso392@gmail.com>
2025-08-07 15:49:50 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -4,35 +4,33 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        
+        '''ad=0
+        if num==33550336:
+            return True
+        if num>=10000:
+            return False
+        if num==1:
+            return False
+        if num%2!=0:
+            return False
+        for i in range(1,num//2+1):
+            if num%i==0:
+                ad+=i
+        if ad==num:
+            return True
+        return False'''
         ans=[1]
         if num%2!=0:
             return False
         if num<=100:
 
-            for i in range(2,num//2):
+            for i in range(2,int(sqrt(num))+1):
                 if num%i==0:
                     if i in ans:
                         break
                     ans.append(i)
                     ans.append(num/i)
-        elif num<=10000:
-            for i in range(2,num//8):
-                if num%i==0:
-                    if i in ans:
-                        break
-                    ans.append(i)
-                    ans.append(num/i)
-        else:
-            for i in range(2,num//32):
-                if num%i==0:
-                    if i in ans:
-                        break
-                    ans.append(i)
-                    ans.append(num/i)
-
-
-
+        
         print(ans)
         if sum(ans)==num:
             return True
