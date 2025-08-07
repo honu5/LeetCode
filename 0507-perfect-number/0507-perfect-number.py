45958e23f1acb18e1107a89920af42a282919386
Honelgn Yohannes <liranso392@gmail.com>
2025-08-07 15:59:59 +0300

    Time: 5 ms (72.26%), Space: 12.6 MB (54.38%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -19,20 +19,16 @@ class Solution(object):
         if ad==num:
             return True
         return False'''
-        ans=[1]
-        if num%2!=0:
+        if num <= 1:
             return False
+    
+        total = 1  # 1 is always a divisor (except for 1 itself)
+        sqrt_n = int(math.sqrt(num))
         
-
-        for i in range(2,int(sqrt(num))+1):
-            if num%i==0:
-                
-                ans.append(i)
-                ans.append(num/i)
+        for i in range(2, sqrt_n + 1):
+            if num % i == 0:
+                total += i
+                if i != num // i:  # Avoid adding square root twice
+                    total += num // i
         
-        print(ans)
-        if sum(ans)==num:
-            return True
-        return False
-
-        
+        return total == num
