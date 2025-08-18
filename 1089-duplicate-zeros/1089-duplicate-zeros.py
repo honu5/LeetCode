826bf3ed7e02efb27fb7e6edb33e42fc046422cc
Honelgn Yohannes <liranso392@gmail.com>
2025-08-18 11:17:57 +0300

    Time: 5 ms (41.5%), Space: 12.6 MB (98.02%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -5,13 +5,12 @@ class Solution(object):
         :rtype: None Do not return anything, modify arr in-place instead.
         """
         l=0
-        r=len(arr)-1
         while l<len(arr):
           if arr[l]==0:
             arr.insert(l+1,0)
             arr.pop()
             l+=2
-            r-=1
+            
           else:
             l+=1
         
