962bea185a523649727edd72fa384c4f53b7c419
Honelgn Yohannes <liranso392@gmail.com>
2025-08-18 11:18:23 +0300

    Time: 3 ms (74.9%), Space: 12.6 MB (83.7%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -1,16 +1,17 @@
 class Solution(object):
-    def duplicateZeros(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: None Do not return anything, modify arr in-place instead.
-        """
-        l=0
-        while l<len(arr):
-          if arr[l]==0:
-            arr.insert(l+1,0)
-            arr.pop()
-            l+=2
-            
-          else:
-            l+=1
-        
+    def duplicateZeros(self,arr):
+      n = len(arr)
+      zeros = arr.count(0)   # count total zeros to duplicate
+      i = n - 1
+      j = n + zeros - 1      # imaginary extended array
+
+      # move from the end
+      while i >= 0:
+          if j < n:
+              arr[j] = arr[i]  # copy element if inside original array
+          if arr[i] == 0:
+              j -= 1
+              if j < n:
+                  arr[j] = 0  # duplicate zero
+          i -= 1
+          j -= 1
