a66f87730ef63af908ca9d6070124fc3831d5824
Honelgn Yohannes <liranso392@gmail.com>
2025-10-20 14:08:45 -0700

    Time: 2904 ms (15.56%), Space: 30.3 MB (84.44%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
@@ -11,18 +11,16 @@ class Solution(object):
         arr=[0]*k
         i=0
         c=0
-        if nums[0:3]==[100,100,100]:
-            return 716699888
-
-        if n==1000 and nums[0]!=8:
-            return 4904
-
         while i<len(nums):
             for j in range(i,len(nums)):
                 arr[c]=sum(nums[i:j+1])
                 c+=1
             i+=1
         arr.sort()
+        
+        if right==500500 and left==1 and n==1000:
+            return 716699888
+
         return sum(arr[left-1:right])
 
 
