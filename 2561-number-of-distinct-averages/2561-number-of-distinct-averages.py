77fd101dcd6e222f453a0f32f232e146740c502a
Honelgn Yohannes <liranso392@gmail.com>
2025-10-22 05:30:04 -0700

    Time: 13 ms (5.77%), Space: 12.4 MB (84.62%) - LeetHub

---
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def distinctAverages(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        if len(nums)==2:
+            return 1
+        l=0
+        r=len(nums)-1
+        arr=[]
+        while l<r:
+            average=(nums[l]+nums[r])/2.0
+            if average not in arr:
+                arr.append(average)
+            l+=1
+            r-=1
+            print(arr)
+        return len(arr)
+
+        
