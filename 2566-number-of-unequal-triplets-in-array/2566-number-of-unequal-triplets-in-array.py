70e1a99bc5821ac70dfeacfc416a1fc1f5ffcf08
Honelgn Yohannes <liranso392@gmail.com>
2025-10-22 05:48:42 -0700

    Time: 410 ms (25.97%), Space: 12.4 MB (81.82%) - LeetHub

---
+++ 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def unequalTriplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                for k in range(j+1,len(nums)):
+                    if nums[i]!=nums[j] and nums[j]!=nums[k] and nums[i]!=nums[k]:
+                        count+=1
+        return count
+
+        
