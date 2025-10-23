8c5ce655324e82233048d1e1a56db228b911a695
Honelgn Yohannes <liranso392@gmail.com>
2025-10-23 14:00:44 -0700

    Time: 3184 ms (5.56%), Space: 23.5 MB (8.33%) - LeetHub

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def maximumBags(self, capacity, rocks, additionalRocks):
+        """
+        :type capacity: List[int]
+        :type rocks: List[int]
+        :type additionalRocks: int
+        :rtype: int
+        """
+        arr=[[rocks[i],capacity[i]] for i in range(len(rocks))]
+        print(arr)
+        ans=[]
+        for i in arr:
+            ans.append(i[1]-i[0])
+        print(ans)
+        ans.sort()
+        i=0
+        final=0
+        while additionalRocks and i<len(ans):
+            additionalRocks-=ans[i]
+            if additionalRocks<0:
+                break
+            
+            
+            i+=1
+            final+=1
+        return final
+            
+        
