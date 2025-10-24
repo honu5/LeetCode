ff4f841ce2da632b8fd21c38f3af4ea1874e1bb1
Honelgn Yohannes <liranso392@gmail.com>
2025-10-24 04:44:43 -0700

    Time: 7 ms (0%), Space: 12.4 MB (84.06%) - LeetHub

---
+++ 2650-split-with-minimum-sum/2650-split-with-minimum-sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def splitNum(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        ans=list(str(num))
+        ans.sort()
+        
+        left=''
+        right=''
+        m=int((len(ans)/2.0)+0.7)
+        for i in range(len(ans)):
+            if i%2==0:
+                left+=ans[i]
+            else:
+                right+=ans[i]
+        print(ans,left,right)
+        return int(left)+int(right)
+        
+        
+
+        
