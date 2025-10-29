e857893cfc42bbb691e0a5897db13a4928d46193
Honelgn Yohannes <liranso392@gmail.com>
2025-10-29 09:05:18 -0700

    Time: 26 ms (8.76%), Space: 12.5 MB (51.96%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -8,8 +8,14 @@ class Solution(object):
             return False
         if len(str(x))==1:
             return True
-        if list(str(x))==list(str(x))[::-1]:
-            return True
-        return False
+        n=len(str(x))
+        l=0
+        r=n-1
+        while l<=r:
+            if str(x)[l]!=str(x)[r]:
+                return False
+            l+=1
+            r-=1
+        return True
 
         
