661fce8eda69828cf08a1cff74cbf9603805161a
Honelgn Yohannes <liranso392@gmail.com>
2025-11-05 23:30:03 +0300

    Time: 6794 ms (5%), Space: 18.4 MB (39.92%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -14,10 +14,7 @@ class Solution(object):
                 if total == 0 and  [nums[i],nums[left],nums[right]] not in res:
                     res.append([nums[i], nums[left], nums[right]])
 
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
+                    
 
                     left += 1
                     right -= 1
