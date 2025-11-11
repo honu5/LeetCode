69f53c93937357d219f4d10c5caf9c9a9525369f
Honelgn Yohannes <liranso392@gmail.com>
2025-11-11 23:38:06 +0300

    Time: 27 ms (78.63%), Space: 12.4 MB (80.15%) - LeetHub

--- 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.py
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type word: str
         :rtype: int
         """
-        vowels=set("aeiou")
+        vowel=set("aieou")
         count=0
         n=len(word)
+
         for i in range(n):
             sett=set()
             for j in word[i:]:
-                if j not in vowels:
+                if j not in vowel:
                     break
                 sett.add(j)
-                if len(sett)>=5:
+                if len(sett)==5:
                     count+=1
         return count
-        
