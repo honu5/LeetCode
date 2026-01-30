ffc62ecc1bc83b7e323f91665ba8f53c9ae695bd
Honelgn Yohannes <liranso392@gmail.com>
2026-01-30 14:38:54 +0300

    Time: 0 ms (100%), Space: 12.5 MB (18.76%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -1,6 +1,8 @@
-class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        far=celsius*1.80+32.00
-        kel=celsius+273.15
-        return [kel,far]
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        return [celsius+273.15,celsius*1.80+32]
         
