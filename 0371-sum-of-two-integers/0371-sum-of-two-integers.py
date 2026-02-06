21e7616322bca90d104d1fb67336da08d3bbacd0
Honelgn Yohannes <liranso392@gmail.com>
2026-02-06 11:12:35 +0300

    Time: 0 ms (100%), Space: 12.4 MB (57.37%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -5,5 +5,23 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        return a+b
+    
+        ans=[0]*2000
+        print(len(ans))
+        if a<0:
+            for i in range(abs(a)):
+                ans.pop()
+        else:
+            for i in range(a):
+                ans.append(0)
+        print(len(ans))
+        if b<0:
+            for i in range(abs(b)):
+                ans.pop()
+        else:
+            for i in range(b):
+                ans.append(0)
+        print(len(ans))
+        return len(ans)-2000
+        
         
