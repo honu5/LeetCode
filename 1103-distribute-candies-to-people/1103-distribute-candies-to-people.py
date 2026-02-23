5c4e65a9106e620cb82a71f635ffaa8bff3b1e33
Honelgn Yohannes <liranso392@gmail.com>
2026-02-23 12:36:57 +0300

    Time: 8 ms (8.45%), Space: 12.6 MB (4.23%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def distributeCandies(self, candies, num_people):
+        """
+        :type candies: int
+        :type num_people: int
+        :rtype: List[int]
+        """
+        ans=[0]*num_people
+        i=1
+        n=candies
+        while n>0:
+            n-=i
+            ans[i%num_people-1]+=i
+            i+=1
+        print(ans,i)
+        if n<0:
+            ans[((i-1)%num_people)-1]+=n
+        
+        return ans
+        
