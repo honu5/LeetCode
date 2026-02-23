ced657ad0b7d85a8cac1d5e0f6dad0b03e77265d
Honelgn Yohannes <liranso392@gmail.com>
2026-02-23 14:19:28 +0300

    Time: 0 ms (100%), Space: 12.8 MB (1.67%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def checkStraightLine(self, coordinates):
+        """
+        :type coordinates: List[List[int]]
+        :rtype: bool
+        """
+        y=[a[0] for a in coordinates]
+        b=[a[1] for a in coordinates]
+        if len(set(y))==1 or len(set(b))==1:
+            return True
+        if len(set(y))!=len(y) or len(set(b))!=len(b):
+            return False
+       
+        if len(coordinates)==2:
+            return True
+        slp=(float(coordinates[1][1])-float(coordinates[0][1]))/(float(coordinates[1][0])-float(coordinates[0][0]))
+        print(slp)
+        for i in range(2,len(coordinates)):
+            if ((float(coordinates[i][1])-float(coordinates[0][1]))/(float(coordinates[i][0])-float(coordinates[0][0])))!=slp:
+                return False
+        return True
+        
