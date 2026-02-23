587c7247a4b54c71f0be48d9659b67c1ab8886b7
Honelgn Yohannes <liranso392@gmail.com>
2026-02-23 12:02:51 +0300

    Time: 0 ms (100%), Space: 12.8 MB (17.49%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -7,12 +7,9 @@ class Solution(object):
         if num%2==1:
             return False
         ans=1
-        lst=[]
         for i in range(2,int(sqrt(num))+1):
-            if num%i==0 and i not in lst:
+            if num%i==0 :
                 ans+=(i+num/i)
-                lst.append(num/i)
-                #print(i,num/i,ans)
         return ans==num
 
         
