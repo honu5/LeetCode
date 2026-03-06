f4117d8dd6363716f85985f21b6b2f29242dce19
Honelgn Yohannes <liranso392@gmail.com>
2026-03-06 15:20:43 +0300

    Time: 0 ms (100%), Space: 19.3 MB (91.13%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -3,10 +3,10 @@ class Solution:
         count1=Counter(word1)
         count2=Counter(word2)
         for i in count1:
-            if (count1[i]>3 and i not in count2) or abs(count1[i]-count2[i])>3:
+            if  abs(count1[i]-count2[i])>3:
                 return False
         for i in count2:
-            if (count2[i]>3 and i not in count1) or abs(count1[i]-count2[i])>3:
+            if  abs(count1[i]-count2[i])>3:
                 return False
         return True
         
