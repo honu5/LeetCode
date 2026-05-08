5bf3d4f08cdb71e34bab0705a2eeff2ebffae70d
Honelgn Yohannes <liranso392@gmail.com>
2026-05-08 12:17:18 +0300

    Time: 0 ms (100%), Space: 19.4 MB (46.81%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        def is_square(x:int)->bool:
+            y=int(x**0.5)
+            return y*y==x
+        if is_square(n):
+            return 1
+        while n%4==0:
+            n//=4
+        if n%8==7:
+            return 4
+        i=1
+        while i*i<=n:
+            if is_square((n-(i*i))):
+                return 2
+            i+=1
+        return 3
+        
